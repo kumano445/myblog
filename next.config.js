b8bb791e2e03b6c5d3f5a7a1e01b92eb9b7e941d
kumano445 <ttkmysk021@gmail.com>
2025-03-19 21:00:02 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone", // "export" を削除
-  webpack: (config) => {
-    config.watchOptions = {
-      poll: 300,
-      aggregateTimeout: 300,
-    };
-    return config;
-  },
+  output: "standalone", 
 };
 
 module.exports = nextConfig;
